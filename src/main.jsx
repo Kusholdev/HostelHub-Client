38d361330a3e6fdbad887ab7e73ac81bf3c60bea
Kushol <ranjansarkerkushol@gmail.com>
2025-10-09 20:32:00 +0600

    adddedFirebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import {RouterProvider} from "react-router";
+import { RouterProvider } from "react-router";
 import { router } from './router/router.jsx';
 
 // Animate on scrolling
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import AuthProvider from './Context/AuthProvider.jsx';
 // Initialize AOS here
 AOS.init({
   duration: 800, // how long animation runs
@@ -18,7 +19,10 @@ AOS.init({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className='font-urbanist max-w-7xl mx-auto'>
-    <RouterProvider router={router}></RouterProvider>
+      <AuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+
+      </AuthProvider>
 
     </div>
   </StrictMode>,
