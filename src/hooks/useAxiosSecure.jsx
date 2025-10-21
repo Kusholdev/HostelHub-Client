7a76f69f54fec4e63c07a7d89d8269c36eeab521
Kushol <ranjansarkerkushol@gmail.com>
2025-10-22 02:13:33 +0600

    addedJWTToken

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,23 @@
 import axios from 'axios';
+import useAuth from './useAuth';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000' // ✅ your backend port
+  baseURL: 'http://localhost:5000' // your backend port
 });
 
 const useAxiosSecure = () => {
+  const { user } = useAuth();
+
+  axiosInstance.interceptors.request.use(async (config) => {
+    if (user) {
+      const token = await user.getIdToken(); // get Firebase JWT
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  }, error => {
+    return Promise.reject(error);
+  });
+
   return axiosInstance;
 };
 
